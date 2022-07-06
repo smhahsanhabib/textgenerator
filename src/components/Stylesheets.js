c698b4f65b4478727acba73906d5d1c56008f5f1
smhahsanhabib <86190186+smhahsanhabib@users.noreply.github.com>
2022-07-07 02:34:02 +0600

    Add files via upload
    CSS updated

---
+++ src/components/Stylesheets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './myStyles.css'
+function Stylesheets() {
+  return (
+    <div>
+        {/* <h1 className='primary'>This is it </h1> */}
+    </div>
+  )
+}
+
+export default Stylesheets
